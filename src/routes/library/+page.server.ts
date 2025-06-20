11595369c87cf024aed3fec0cb39822565a893d9
Logan <Logan@cattn.dev>
2025-06-20 17:58:50 -0400

    update fetch locations

--- src/routes/library/+page.server.ts
+++ src/routes/library/+page.server.ts
@@ -2,6 +2,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
 	return {
-		files: await fetch(url.protocol + '//' + url.hostname + ':5823/api/getFiles').then(res => res.json())
+		files: await fetch('http://' + url.hostname + ':5823/api/getFiles').then(res => res.json())
 	};
 };	
--- src/routes/upload/+page.server.ts
+++ src/routes/upload/+page.server.ts
@@ -2,9 +2,9 @@ import type { Actions } from './$types';
 
 export const actions = {
 	default: async (event) => {
-        console.log(event.url.protocol + '//' + event.url.hostname + ':5823/api/upload');
+        console.log('http://' + event.url.hostname + ':5823/api/upload');
         const formData = await event.request.formData();
-        const response = await fetch(event.url.protocol + '//' + event.url.hostname + ':5823/api/upload', {
+        const response = await fetch('http://' + event.url.hostname + ':5823/api/upload', {
             method: 'POST',
             body: formData
         });
